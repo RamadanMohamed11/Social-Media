beb638d20c30f32efd69fb5a80bdf20a91367d57
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-10-09 00:26:50 +0300

    finsih AddView

--- lib/features/add/presentation/views/add_view.dart
+++ lib/features/add/presentation/views/add_view.dart
@@ -6,6 +6,20 @@ class AddView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: AddViewModel());
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        title: Row(
+          children: [
+            const Text("Add Post"),
+            const Spacer(),
+            TextButton(onPressed: () {}, child: const Text("Post")),
+          ],
+        ),
+      ),
+      body: const AddViewModel(),
+    );
   }
 }
--- lib/features/add/presentation/views/widgets/add_view_model.dart
+++ lib/features/add/presentation/views/widgets/add_view_model.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:social_media/constants.dart';
+import 'package:social_media/core/utils/app_colors.dart';
+import 'package:social_media/core/utils/assets_data.dart';
 
 class AddViewModel extends StatelessWidget {
   const AddViewModel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Add");
+    return Padding(
+      padding: EdgeInsets.all(kDefaultPadding),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(backgroundImage: AssetImage(AssetsData.man)),
+              Gap(10),
+              Expanded(
+                child: TextFormField(
+                  maxLines: 5,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "What's on your mind?",
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Spacer(),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.all(10),
+              shape: const CircleBorder(),
+              backgroundColor: AppColors.ksecondaryColor,
+            ),
+            onPressed: () {},
+            child: Icon(
+              Icons.camera_alt_outlined,
+              color: AppColors.kWhiteColor,
+              size: 25,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
