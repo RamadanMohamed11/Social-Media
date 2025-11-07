c363ca0abe1c1cdacd0f20805075ab5552279e40
Ramadan Mohamed <elking.medo61111@gmail.com>
2025-11-07 15:35:29 +0200

    Refactor HomeViewBody to use StatefulWidget, implement scroll controller, and optimize user model fetching

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -13,15 +13,41 @@ import 'package:social_media/features/home/presentation/view_model/home_view_cub
 import 'package:social_media/features/home/presentation/views/widgets/post_widget.dart';
 import 'package:social_media/features/profile/presentation/view_model/cubit/profile_cubit.dart';
 
-class HomeViewBody extends StatelessWidget {
+class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key, required this.userModel});
   final UserModel userModel;
 
-  Future<UserModel> getThePostUserModel({required String uid}) async {
+  @override
+  State<HomeViewBody> createState() => _HomeViewBodyState();
+}
+
+class _HomeViewBodyState extends State<HomeViewBody> {
+  late final ScrollController _scrollController;
+  late final Stream<List<PostModel>> _postsStream;
+  final Map<String, Future<UserModel>> _userFutureCache = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _postsStream = context.read<HomeViewCubit>().getPosts();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<UserModel> _fetchPostUser(String uid) async {
     final DocumentSnapshot doc = await getIt
         .get<CloudService>(instanceName: 'users')
         .getDataById(docId: uid);
     return UserModel.fromSnap(doc);
+  }
+
+  Future<UserModel> _getPostUserFuture(String uid) {
+    return _userFutureCache.putIfAbsent(uid, () => _fetchPostUser(uid));
   }
 
   @override
@@ -40,12 +66,14 @@ class HomeViewBody extends StatelessWidget {
         }
       },
       child: StreamBuilder<List<PostModel>>(
-        stream: BlocProvider.of<HomeViewCubit>(context).getPosts(),
+        stream: _postsStream,
         builder: (context, asyncSnapshot) {
           if (asyncSnapshot.connectionState == ConnectionState.waiting) {
             return Padding(
               padding: const EdgeInsets.all(kDefaultPadding),
               child: ListView.builder(
+                key: const PageStorageKey('home_skeleton_list'),
+                controller: _scrollController,
                 itemCount: 10,
                 itemBuilder: (context, index) {
                   return const PostWidget().redacted(
@@ -74,11 +102,13 @@ class HomeViewBody extends StatelessWidget {
               return Padding(
                 padding: const EdgeInsets.all(kDefaultPadding),
                 child: ListView.builder(
+                  key: const PageStorageKey('home_posts_list'),
+                  controller: _scrollController,
                   itemCount: posts.length,
                   itemBuilder: (context, index) {
                     final post = posts[index];
                     return FutureBuilder(
-                      future: getThePostUserModel(uid: post.uid),
+                      future: _getPostUserFuture(post.uid),
                       builder: (context, asyncSnapshot) {
                         if (asyncSnapshot.connectionState ==
                             ConnectionState.waiting) {
@@ -86,13 +116,15 @@ class HomeViewBody extends StatelessWidget {
                             context: context,
                             redact: true,
                             configuration: RedactedConfiguration(
-                              animationDuration: const Duration(milliseconds: 500),
+                              animationDuration: const Duration(
+                                milliseconds: 500,
+                              ),
                             ),
                           );
                         } else if (asyncSnapshot.hasData) {
                           final UserModel postUserModel = asyncSnapshot.data!;
                           return PostWidget(
-                            userModel: userModel,
+                            userModel: widget.userModel,
                             postUserModel: postUserModel,
                             post: post,
                             onOwnerTap: () {
@@ -106,12 +138,13 @@ class HomeViewBody extends StatelessWidget {
                               final currentUser = await profileCubit.profileRepo
                                   .getCurrentUser();
 
-                              if (post.likes.contains(currentUser.uid)) {
-                                post.likes.remove(currentUser.uid);
-                              } else {
-                                post.likes.add(currentUser.uid);
-                              }
-
+                              setState(() {
+                                if (post.likes.contains(currentUser.uid)) {
+                                  post.likes.remove(currentUser.uid);
+                                } else {
+                                  post.likes.add(currentUser.uid);
+                                }
+                              });
                               await BlocProvider.of<HomeViewCubit>(
                                 context,
                               ).lovePost(newPost: post);
